ca2880772e9b64e7a8bc767bdba5f3a4d088fe07
omkar hingne <105487031+hingneonkar@users.noreply.github.com>
2025-05-29 23:39:48 +0530

    Add files via upload

--- Server/routes/submissionRoutes.js
+++ Server/routes/submissionRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { createSubmission, getAllSubmissions } = require('../controllers/submissionController');
 
+router.post('/submission', createSubmission);
+router.get('/submission', getAllSubmissions);
+
+module.exports = router;
